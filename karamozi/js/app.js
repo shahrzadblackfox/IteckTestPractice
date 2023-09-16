ec3be26d50492a1c571153bccd3fd3c63e913095
Shahrzad <75183431+shahrzadblackfox@users.noreply.github.com>
2023-09-16 11:44:45 +0330

    adding owl carousel

---
+++ karamozi/js/app.js
@@ -0,0 +1,18 @@
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    rtl: true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
